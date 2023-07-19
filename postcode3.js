f779a224fb7f23d2e8f3a62a006208240931d6f6
locaseluke <74836417+locaseluke@users.noreply.github.com>
2023-07-19 13:09:44 +1000

    Update postcode3.js

--- postcode3.js
+++ postcode3.js
@@ -1,4 +1,28 @@
 
+function setCookie(name, value, days) {
+  const expirationDate = new Date();
+  expirationDate.setTime(expirationDate.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + expirationDate.toUTCString();
+  const domain = "domain=amber.com.au";
+  document.cookie = name + "=" + encodeURIComponent(value.value) + "; " + expires + "; path=/; " + domain;
+}
+
+function getCookie(name) {
+  const cookieName = name + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(";");
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === " ") {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return "";
+}
+
 $(document).ready(function () {
   $(".post-code-input").attr("maxlength", "4");
 });
