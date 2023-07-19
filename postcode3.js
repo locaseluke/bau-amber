b5311b45745b6ab5bc5af075242c33648dcca93c
locaseluke <74836417+locaseluke@users.noreply.github.com>
2023-07-19 13:28:53 +1000

    Update postcode3.js

--- postcode3.js
+++ postcode3.js
@@ -1,28 +1,26 @@
+function setCookie(name, value, days) {
+  const expirationDate = new Date();
+  expirationDate.setTime(expirationDate.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + expirationDate.toUTCString();
+  const domain = "domain=amber.com.au";
+  document.cookie = name + "=" + encodeURIComponent(value.value) + "; " + expires + "; path=/; " + domain;
+}
 
-  // get the value of URL parameter
-  function getURLParameter(name) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(name);
-  }
-
-  // set the COUPONCODE cookie with the value of 'couponcode' parameter
-  function setCouponCodeCookie() {
-    const couponCodeValue = getURLParameter('couponcode');
-    if (couponCodeValue) {
-      // Set the COUPONCODE cookie with the extracted value
-      document.cookie = 'COUPONCODE=' + encodeURIComponent(couponCodeValue) + '; path=/; domain=amber.com.au; expires=' + getCookieExpiration();
+function getCookie(name) {
+  const cookieName = name + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(";");
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === " ") {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
     }
   }
-
-  // get the expiration date for the cookie 
-  function getCookieExpiration() {
-    const expirationDate = new Date();
-    expirationDate.setTime(expirationDate.getTime() + 7 * 24 * 60 * 60 * 1000); // 7 days in milliseconds
-    return expirationDate.toUTCString();
-  }
-
-  setCouponCodeCookie();
-
+  return "";
+}
 
 $(document).ready(function () {
   $(".post-code-input").attr("maxlength", "4");
